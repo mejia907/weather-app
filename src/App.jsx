f91aa4b7c940863c9b40ea93327093e2e2ce381c
Andres Felipe Mejia <mejia9070@gmail.com>
2023-03-04 13:18:17 -0500

    Ajsutes en diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { LoadingButton } from '@mui/lab'
-import { Box, Container, TextField, Typography } from '@mui/material'
+import { Box, Container, TextField, Typography, styled } from '@mui/material'
 import { useState } from 'react'
 
 
@@ -17,6 +17,32 @@ const INITIAL_ERROR = {
   message: ''
 }
 
+const CssTextField = styled(TextField)({
+  '& .MuiInputBase-input':{
+    color: 'white',
+  },
+  '& label.Mui-focused': {
+    color: 'white',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'white',
+  },
+  '& .MuiInputLabel-root': {
+    color: 'white',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'white',
+    },
+    '&:hover fieldset': {
+      borderColor: 'white',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'white',
+    },
+  },
+});
+
 const API_WEATHER = `${import.meta.env.VITE_API_URL}?key=${import.meta.env.VITE_API_KEY}&q=`
 
 function App() {
@@ -31,7 +57,7 @@ function App() {
     setLoading(true)
     setError(INITIAL_ERROR)
     try {
-      if(!city.trim()) throw { message: 'Debe digitar una ciudad' }
+      if(!city.trim()) throw { message: 'You must enter a city' }
       const response = await fetch(`${API_WEATHER}${city}`)
       const data = await response.json()
       if(data.error) throw { message: data.error.message }
@@ -57,18 +83,18 @@ function App() {
   }
 
   return (
-    <Container maxWidth='xs' sx={{ mt:2 }}>
-      <Typography variant='h3' component='h1' align='center' gutterBottom>
+    <Container maxWidth='xs' sx={{ mt:2, p:2, backgroundColor:'#00000040', borderRadius:'10px' }}>
+      <Typography variant='h3' component='h1' align='center' sx={{ color:'white' }} gutterBottom>
         Weather APP
       </Typography>
       <Box sx={{ display:'grid', gap:2 }} component='form' autoComplete='off' onSubmit={handleSearchCity}>
-        <TextField value={city} id='city' label='Ciudad' variant='outlined' required fullWidth onChange={(event) => setCity(event.target.value)} error={error.error} helperText={error.message} />
-        <LoadingButton type='submit' variant='contained' loading={loading} loadingIndicator='Cargando...'>
-          Buscar
+        <CssTextField value={city} id='city' label='City' variant='outlined' required fullWidth onChange={(event) => setCity(event.target.value)} error={error.error} helperText={error.message} />
+        <LoadingButton type='submit' variant='contained' loading={loading} loadingIndicator='Loading...'>
+          Search
         </LoadingButton>
       </Box>
       {weather.city && (
-        <Box sx={{ mt:2, display:'grid', gap:2, textAlign:'center' }}>
+        <Box sx={{ mt:2, display:'grid', gap:2, textAlign:'center', color:'white' }}>
           <Typography variant='h4' component='h2'>
             {weather.city}, {weather.country}
           </Typography>
@@ -81,7 +107,7 @@ function App() {
           </Typography>
         </Box>
       )}
-      <Typography textAlign='center' sx={{ mt:2, fontSize: '12px'}}>
+      <Typography textAlign='center' sx={{ mt:2, fontSize: '12px', color: 'white'}}>
         Powered by : {''}
         <a href='https://www.weatherapi.com/' title='Weather API'>WeatherAPI.com</a>
       </Typography>
